2e4173c43fb108f05494869efd4d3a11449084b0
Gh0se4 <Gh0se4@github.com>
2026-07-17 10:05:58 +0800

    reduce DO_FETCH timeout to 3s

--- entrypoints/bm-capture.content.ts
+++ entrypoints/bm-capture.content.ts
@@ -691,7 +691,7 @@ function mainWorldFetch(opts: {
     setTimeout(() => {
       window.removeEventListener('message', handler);
       reject(new Error("MAIN world fetch timeout"));
-    }, 8000);
+    }, 3000);
   });
 }
 
